a8a0aeb702e7c5b0164f54a317746556ae9efc7f
fadyashraf8 <fadyashraf909@gmail.com>
2025-11-10 23:12:22 +0200

    set up the project

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 import express from "express";
+import { AppError } from "./utils/AppError";
+import { globalMiddleWare } from "./middleware/globalMiddleWare";
 const app = express();
 const port = 3000;
 
+app.all("*", (req, res, next) => {
+  next(new AppError(`can't find this route: ${req.originalUrl}`, 404));
+});
+
+app.use(globalMiddleWare);
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
