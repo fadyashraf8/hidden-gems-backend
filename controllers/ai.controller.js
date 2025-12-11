71b793d7b384b1a0dfe534d2142d2230f1ca8e71
Zeyad Mohamed <ziadmohomar123@gmail.com>
2025-12-11 19:10:07 +0200

    add: benchmark for ai gem picks

--- controllers/ai.controller.js
+++ controllers/ai.controller.js
@@ -13,21 +13,20 @@ const generateSuggestions = catchAsyncError(async (req, res, next) => {
     // console.log(s.gem.name, "score:", cosineSimilarity(userVector, s.gem.embeddings.values));
 
     similarties.sort((a,b) => b.score - a.score);
-//     const THRESHOLD = 0.60;
-//   const relevant = similarties.filter(s => s.score >= THRESHOLD);
-
-//   if (relevant.length === 0) {
-//     return res.status(200).json({
-//       suggestions: null,
-//       allSuggestions: [],
-//       message: "No relevant gems found for this prompt",
-//     });
-//   }
+    const THRESHOLD = 0.70;
+  const relevant = similarties.filter(s => s.score >= THRESHOLD);
     const matches = similarties.slice(0, 3).map(s => {
             const {embeddings, ...gem} = s.gem.toObject();
             return gem;
-        }); 
-    const firstMatch = matches.shift();    
+        });
+    if (relevant.length === 0) {
+    return res.status(200).json({
+      suggestions: null,
+      allSuggestions: matches,
+      message: "No relevant gems found in our website but you could like these picks.",
+    });
+  }     
+    const firstMatch = matches.shift();   
     return res.status(200).json({
         suggestions: firstMatch,
         allSuggestions: matches,
