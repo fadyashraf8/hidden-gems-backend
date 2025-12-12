711f10d486710e21a0e35a5dd931b2d87f10a59c
fadyashraf8 <fadyashraf909@gmail.com>
2025-12-12 13:55:29 +0200

    edit in review repo

--- repository/review.repository.js
+++ repository/review.repository.js
@@ -1,30 +1,41 @@
 import mongoose from "mongoose";
 import { reviewModel } from "../models/review.js";
 
-
 export const createReview = async (review) => {
-    return (await reviewModel.create(review))
-        .populate({path: "userId", select: "firstName lastName"});
-}
+  const created = await reviewModel.create(review);
+  return created.populate("userId", "firstName lastName");
+};
+
 
 export const getAllReviews = () => {
-    return reviewModel.find().populate({path: "userId", select: "firstName lastName"});
-}
+  return reviewModel
+    .find()
+        .populate("userId", "firstName lastName email")
+    .populate("gemId", "-embeddings");
+};
 
 export const getReviewByAuthorId = async (id, gemId) => {
-    return reviewModel.findOne({userId: id, gemId: gemId});
-}
+  return reviewModel.findOne({ userId: id, gemId: gemId })        .populate("userId", "firstName lastName email")
+    .populate("gemId", "-embeddings");
+};
 
 export const getAllReviewsForGem = (gemId) => {
-    return reviewModel.find({gemId: gemId})
-        .populate({path: "userId", select: "firstName lastName"});
-}
+  return reviewModel
+    .find({ gemId: gemId })
+        .populate("userId", "firstName lastName email")
+    .populate("gemId", "-embeddings");
+};
 
 export const deleteReviewById = async (id) => {
-    return await reviewModel.findByIdAndDelete(id);
-}
+  return await reviewModel.findByIdAndDelete(id);
+};
 
 export const updateReviewById = async (id, updatedFields) => {
-    return await reviewModel.findByIdAndUpdate(id,  {$set: updatedFields }, {new: true});
-}
-
+  return await reviewModel.findByIdAndUpdate(
+    id,
+    { $set: updatedFields },
+    { new: true }
+  )
+          .populate("userId", "firstName lastName email")
+    .populate("gemId", "-embeddings");
+};
